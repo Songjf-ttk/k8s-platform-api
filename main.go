a0c1745eeb3506cbdfca451425e5f96cd197cebb
qqmiller <13917099322@139.com>
2023-03-08 05:23:38 +0000

    k8s api

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"k8s-plantform/config"
+	"k8s-plantform/controller"
+	"k8s-plantform/db"
+	"k8s-plantform/middle"
+	"k8s-plantform/service"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// 初始化数据库
+	db.Init()
+	// 初始化k8s client
+	service.K8s.Init()
+	// 初始化gin
+	r := gin.Default()
+	// 加载jwt中间件
+	//r.Use(middle.JWTAuth())
+	// 加载跨域中间件
+	r.Use(middle.Cors())
+	// 初始化路由
+	controller.Router.InitApiRouter(r)
+	//启动websocket
+	go func() {
+		http.HandleFunc("/ws", service.Terminal.WsHandler)
+		http.ListenAndServe(":8081", nil)
+	}()
+	// gin 程序启动
+	fmt.Println("http://192.168.31.1:9091/")
+	r.Run(config.ListenAddr)
+	// 关闭数据库
+	db.Close()
+}
---
+++ service/init.go
@@ -0,0 +1,35 @@
+package service
+
+import (
+	"k8s-plantform/config"
+
+	"github.com/wonderivan/logger"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+// 用于初始化k8s client
+var K8s k8s
+
+type k8s struct {
+	ClientSet *kubernetes.Clientset
+}
+
+// 初始化k8s
+func (k *k8s) Init() {
+	// 将kuberconfig文件转换成rest.config对象
+	conf, err := clientcmd.BuildConfigFromFlags("", config.Kubeconfig)
+	if err != nil {
+		panic("获取K8s配置失败:" + err.Error())
+	} else {
+		logger.Info("获取K8s配置 成功!")
+	}
+	// 根据rest.config对象,new一个clientset出来
+	clientset, err := kubernetes.NewForConfig(conf)
+	if err != nil {
+		panic("创建K8s client失败:" + err.Error())
+	} else {
+		logger.Info("创建K8s client 成功!")
+	}
+	k.ClientSet = clientset
+}
